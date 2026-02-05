d0833405a216be2ca5cd99ed29d9a82ce6cd8b29
Danyel Torres <danyeltorres212@gmail.com>
2026-02-05 15:04:03 -0500

    Fix: cambio a puerto 465 SSL para evitar timeout

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Configuración de Correo
 app.config['MAIL_SERVER'] = '74.125.141.108'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
